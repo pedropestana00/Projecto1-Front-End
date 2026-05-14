0f2cf54e5b0a21f4965433de8439cfa27b01edf3
pedropestana00 <pedropestana_@hotmail.com>
2026-05-14 18:23:15 +0100

    Update firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -8,13 +8,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyB-7CX68G3O3cx-pvJtN5Knr9W1BYUmkr8",
-  authDomain: "projeto2526-6a582.firebaseapp.com",
-  projectId: "projeto2526-6a582",
-  storageBucket: "projeto2526-6a582.firebasestorage.app",
-  messagingSenderId: "1048877891409",
-  appId: "1:1048877891409:web:b99705e3c51a27eb249d70",
-  measurementId: "G-TRMW5J6HV2"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 // Initialize Firebase
